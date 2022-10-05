2c1b73fd7a7e1b79bcf799fb2052c726b40033cb
wlpdrpat <wlpdrpat@gmail.com>
2022-10-04 23:24:34 -0400

    Update and rename class-plugin-name-admin.php to class-excerpt-thumbnail-admin.php

--- excerpt-thumbnail/admin/class-excerpt-thumbnail-admin.php
+++ excerpt-thumbnail/admin/class-excerpt-thumbnail-admin.php
@@ -3,11 +3,11 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * @link       http://example.com
+ * @link       http://wellplanet.com
  * @since      1.0.0
  *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin
+ * @package    Excerpt_Thumbnail
+ * @subpackage Excerpt_Thumbnail/admin
  */
 
 /**
@@ -16,20 +16,20 @@
  * Defines the plugin name, version, and two examples hooks for how to
  * enqueue the admin-specific stylesheet and JavaScript.
  *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin
- * @author     Your Name <email@example.com>
+ * @package    Excerpt_Thumbnail
+ * @subpackage Excerpt_Thumbnail/admin
+ * @author     Patrick Coleman <pat@wellplanet.com>
  */
-class Plugin_Name_Admin {
+class Excerpt_Thumbnail_Admin {
 
 	/**
 	 * The ID of this plugin.
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
+	 * @var      string    $excerpt_thumbnail    The ID of this plugin.
 	 */
-	private $plugin_name;
+	private $excerpt_thumbnail;
 
 	/**
 	 * The version of this plugin.
@@ -44,12 +44,12 @@ class Plugin_Name_Admin {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $excerpt_thumbnail       The name of this plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct( $plugin_name, $version ) {
+	public function __construct( $excerpt_thumbnail, $version ) {
 
-		$this->plugin_name = $plugin_name;
+		$this->excerpt_thumbnail = $excerpt_thumbnail;
 		$this->version = $version;
 
 	}
@@ -65,15 +65,15 @@ class Plugin_Name_Admin {
 		 * This function is provided for demonstration purposes only.
 		 *
 		 * An instance of this class should be passed to the run() function
-		 * defined in Plugin_Name_Loader as all of the hooks are defined
+		 * defined in Excerpt_Thumbnail_Loader as all of the hooks are defined
 		 * in that particular class.
 		 *
-		 * The Plugin_Name_Loader will then create the relationship
+		 * The Excerpt_Thumbnail_Loader will then create the relationship
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/plugin-name-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->excerpt_thumbnail, plugin_dir_url( __FILE__ ) . 'css/excerpt-thumbnail-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -88,15 +88,15 @@ class Plugin_Name_Admin {
 		 * This function is provided for demonstration purposes only.
 		 *
 		 * An instance of this class should be passed to the run() function
-		 * defined in Plugin_Name_Loader as all of the hooks are defined
+		 * defined in Excerpt_Thumbnail_Loader as all of the hooks are defined
 		 * in that particular class.
 		 *
-		 * The Plugin_Name_Loader will then create the relationship
+		 * The Excerpt_Thumbnail_Loader will then create the relationship
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->excerpt_thumbnail, plugin_dir_url( __FILE__ ) . 'js/excerpt-thumbnail-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
