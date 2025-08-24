26b554b9999107b273226d35f3c745a57158753d
wlpdrpat <wlpdrpat@gmail.com>
2025-08-24 16:55:06 +0100

    Update class-excerpt-thumbnail-public.php
    1st commit after boilerplate

--- excerpt-thumbnail/public/class-excerpt-thumbnail-public.php
+++ excerpt-thumbnail/public/class-excerpt-thumbnail-public.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The public-facing functionality of the plugin.
  *
@@ -21,83 +20,14 @@
  * @author     Patrick Coleman <pat@wellplanet.com>
  */
 class Excerpt_Thumbnail_Public {
+    private $slug;
+    private $version;
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $excerpt_thumbnail    The ID of this plugin.
-	 */
-	private $excerpt_thumbnail;
+    public function __construct( $slug, $version ) {
+        $this->slug    = $slug;
+        $this->version = $version;
+    }
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
-
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $excerpt_thumbnail       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $excerpt_thumbnail, $version ) {
-
-		$this->excerpt_thumbnail = $excerpt_thumbnail;
-		$this->version = $version;
-
-	}
-
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Excerpt_Thumbnail_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Excerpt_Thumbnail_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->excerpt_thumbnail, plugin_dir_url( __FILE__ ) . 'css/excerpt-thumbnail-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Excerpt_Thumbnail_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Excerpt_Thumbnail_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->excerpt_thumbnail, plugin_dir_url( __FILE__ ) . 'js/excerpt-thumbnail-public.js', array( 'jquery' ), $this->version, false );
-
-	}
-
+    public function enqueue_styles() {}
+    public function enqueue_scripts() {}
 }
