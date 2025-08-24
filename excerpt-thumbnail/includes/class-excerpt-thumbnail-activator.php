34408b34d73bf1815bbaf09f818d18ce710e67ca
wlpdrpat <wlpdrpat@gmail.com>
2025-08-24 17:18:05 +0100

    Update class-excerpt-thumbnail-activator.php
    header update

--- excerpt-thumbnail/includes/class-excerpt-thumbnail-activator.php
+++ excerpt-thumbnail/includes/class-excerpt-thumbnail-activator.php
@@ -1,26 +1,28 @@
 <?php
-
-/**
- * Fired during plugin activation
- *
- * @link       http://wellplanet.com
- * @since      1.0.0
- *
- * @package    Excerpt_Thumbnail
- * @subpackage Excerpt_Thumbnail/includes
- */
-
 /**
  * Fired during plugin activation.
  *
- * This class defines all code necessary to run during the plugin's activation.
- *
- * @since      1.0.0
+ * @link       https://wellplanet.com
+ * @since      3.0.0
  * @package    Excerpt_Thumbnail
  * @subpackage Excerpt_Thumbnail/includes
  * @author     Patrick Coleman <pat@wellplanet.com>
+ * @license    GPL-2.0-or-later
+ */
+
+/**
+ * Defines all code executed during plugin activation.
+ *
+ * @since 3.0.0
  */
 class Excerpt_Thumbnail_Activator {
+
+    /**
+     * Run activation routines.
+     *
+     * @since 3.0.0
+     * @return void
+     */
     public static function activate() {
         // Nothing yet.
     }
