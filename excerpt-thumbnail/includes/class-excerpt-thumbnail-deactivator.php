658f8f55cf678f83db183b9ce14bff510058024b
wlpdrpat <wlpdrpat@gmail.com>
2025-08-24 17:18:40 +0100

    Update class-excerpt-thumbnail-deactivator.php
    header update

--- excerpt-thumbnail/includes/class-excerpt-thumbnail-deactivator.php
+++ excerpt-thumbnail/includes/class-excerpt-thumbnail-deactivator.php
@@ -1,26 +1,28 @@
 <?php
-
-/**
- * Fired during plugin deactivation
- *
- * @link       http://wellplanet.com
- * @since      1.0.0
- *
- * @package    Excerpt_Thumbnail
- * @subpackage Excerpt_Thumbnail/includes
- */
-
 /**
  * Fired during plugin deactivation.
  *
- * This class defines all code necessary to run during the plugin's deactivation.
- *
- * @since      1.0.0
+ * @link       https://wellplanet.com
+ * @since      3.0.0
  * @package    Excerpt_Thumbnail
  * @subpackage Excerpt_Thumbnail/includes
  * @author     Patrick Coleman <pat@wellplanet.com>
+ * @license    GPL-2.0-or-later
+ */
+
+/**
+ * Defines all code executed during plugin deactivation.
+ *
+ * @since 3.0.0
  */
 class Excerpt_Thumbnail_Deactivator {
+
+    /**
+     * Run deactivation routines.
+     *
+     * @since 3.0.0
+     * @return void
+     */
     public static function deactivate() {
         // Nothing yet.
     }
