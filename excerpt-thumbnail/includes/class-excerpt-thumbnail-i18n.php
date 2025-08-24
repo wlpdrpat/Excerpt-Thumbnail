883858d419c5de9f0aa1e023a92913b1c5616d6f
wlpdrpat <wlpdrpat@gmail.com>
2025-08-24 16:49:12 +0100

    Update class-excerpt-thumbnail-i18n.php
    First commit after boilerplate

--- excerpt-thumbnail/includes/class-excerpt-thumbnail-i18n.php
+++ excerpt-thumbnail/includes/class-excerpt-thumbnail-i18n.php
@@ -1,46 +1,16 @@
 <?php
-
-/**
- * Define the internationalization functionality
- *
- * Loads and defines the internationalization files for this plugin
- * so that it is ready for translation.
- *
- * @link       http://wellplanet.com
- * @since      1.0.0
- *
- * @package    Excerpt_Thumbnail
- * @subpackage Excerpt_Thumbnail/includes
- */
-/**
- * Define the internationalization functionality.
- *
- * Loads and defines the internationalization files for this plugin
- * so that it is ready for translation.
- *
- * @since      1.0.0
- * @package    Excerpt_Thumbnail
- * @subpackage Excerpt_Thumbnail/includes
- * @author     Patrick Coleman <pat@wellplanet.com>
- */
 class Excerpt_Thumbnail_i18n {
+    private $domain;
 
+    public function set_domain( $domain ) {
+        $this->domain = $domain;
+    }
 
-	/**
-	 * Load the plugin text domain for translation.
-	 *
-	 * @since    1.0.0
-	 */
-	public function load_plugin_textdomain() {
-
-		load_plugin_textdomain(
-			'excerpt-thumbnail',
-			false,
-			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
-		);
-
-	}
-
-
-
+    public function load_plugin_textdomain() {
+        load_plugin_textdomain(
+            $this->domain,
+            false,
+            dirname( plugin_basename( EXCERPT_THUMBNAIL_FILE ) ) . '/languages/'
+        );
+    }
 }
