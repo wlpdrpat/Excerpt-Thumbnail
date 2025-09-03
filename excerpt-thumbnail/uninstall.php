6e4bf8825af11cb435cbb5ac9da3e15060a5a58e
wlpdrpat <wlpdrpat@gmail.com>
2025-09-03 17:44:34 +0100

    Update uninstall.php
    preparing for WP repository submission.

--- excerpt-thumbnail/uninstall.php
+++ excerpt-thumbnail/uninstall.php
@@ -6,38 +6,44 @@
  * "Remove Data on Uninstall" setting.
  *
  * @link       https://wellplanet.com
- * @since      3.0.0
+ * @since      1.0.0
  * @package    Excerpt_Thumbnail
- * @author     Patrick Coleman <pat@wellplanet.com>
+ * @author     Patrick Coleman
  * @license    GPL-2.0-or-later
  */
 
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit;
+	exit;
 }
 
-// Only remove data if the user explicitly opted in.
-if ( 'yes' !== get_option( 'tfe_cleanup_on_uninstall', 'no' ) ) {
-    return;
+/**
+ * Only remove data if the user explicitly opted in.
+ * This setting should be stored as 'excerpt_thumbnail_cleanup_on_uninstall'.
+ */
+if ( 'yes' !== get_option( 'excerpt_thumbnail_cleanup_on_uninstall', 'no' ) ) {
+	return;
 }
 
-// List all options this plugin creates.
+/**
+ * List all options this plugin creates.
+ * Update this list if new options are added in future versions.
+ */
 $option_keys = [
-    'tfe_width',
-    'tfe_height',
-    'tfe_align',
-    'tfe_default_image',
-    'tfe_default_image_src',
-    'tfe_withlink',
-    'tfe_on_home',
-    'tfe_on_archives',
-    'tfe_on_search',
-    'tfe_exclusion',
-    'tfe_add_og_image',
-    'tfe_modern_mode',
-    'tfe_cleanup_on_uninstall',
+	'excerpt_thumbnail_width',
+	'excerpt_thumbnail_height',
+	'excerpt_thumbnail_align',
+	'excerpt_thumbnail_default_image',
+	'excerpt_thumbnail_default_image_src',
+	'excerpt_thumbnail_withlink',
+	'excerpt_thumbnail_on_home',
+	'excerpt_thumbnail_on_archives',
+	'excerpt_thumbnail_on_search',
+	'excerpt_thumbnail_exclusion',
+	'excerpt_thumbnail_add_og_image',
+	'excerpt_thumbnail_modern_mode',
+	'excerpt_thumbnail_cleanup_on_uninstall',
 ];
 
 foreach ( $option_keys as $key ) {
-    delete_option( $key );
+	delete_option( $key );
 }
